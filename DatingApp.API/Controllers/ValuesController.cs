a742fce4e2929c868b70f972a5f2ae7ea86aaaf5
mojtaba622 <mojtaba622@gmail.com>
2019-07-03 01:07:09 +0430

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,7 +21,8 @@ namespace DatingApp.API.Controllers
             _context = context;
 
         }
-        // GET api/values
+        
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
