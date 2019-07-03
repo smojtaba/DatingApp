9d085e99ac88f8526b376c8322dee4d31b7be977
mojtaba622 <mojtaba622@gmail.com>
2019-07-03 22:42:06 +0430

    End of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,25 +9,24 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alrtify: AlertifyService) { }
 
   ngOnInit() {
   }
   login() {
     this.authService.login(this.model).subscribe(next => {
-      console.log('logged in successfully');
+      this.alrtify.success('logged in successfully');
     }, error => {
-      console.log(error);
+      this.alrtify.error(error);
     });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alrtify.message('logged out');
   }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +10,21 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
 @Output() cancelRegister = new EventEmitter();
 model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alrtify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register() {
     this.authService.register(this.model).subscribe(() => {
-      console.log('registeration successful');
+      this.alrtify.success('registeration successful');
     }, error => {
-      console.log(error);
+      this.alrtify.error(error);
     });
   }
 
   cancel() {
-    this.cancelRegister.emit(false);
-    console.log('cancel');
+    this.cancelRegister.emit(false);    
   }
 
 }
